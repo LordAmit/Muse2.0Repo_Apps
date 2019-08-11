f0c11783e9b01d5fa40309d5e60438263ddde7b6
Amit <amitsealami@gmail.com>
2019-08-11 15:48:05 -0400

    removed unnecessary test files

--- test/run.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-java -jar /home/amit/workspaces/muse/Muse.jar /home/amit/workspaces/muse/libs4ast /home/amit/muse/output/test/test_src/ Test /home/amit/muse/output/test/output/ TAINTSINK > CalendarTrigger_TAINTSINK.log
-echo finished executing TAINTSINK
